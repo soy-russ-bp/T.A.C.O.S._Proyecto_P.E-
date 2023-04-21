64bbebec71e6908de2d06a4536bb60d84b8acb4d
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2023-04-21 02:07:06 -0600

    Orders - TotalOrderCount, OpenOrderCount

--- TACOS-SRC/TACOS/Include/Orders.h
+++ TACOS-SRC/TACOS/Include/Orders.h
@@ -15,6 +15,9 @@ typedef struct {
 	//int* foodList; // TODO: Remplazar tipo por lista de alimentos
 } Table;
 
+// Obtiene la cantidad de ordenes abiertas actualmente.
+UINT Orders_GetOpenCount(void);
+
 // Obtiene la mesa en base a su indice.
 Table* Orders_GetTableByIndex(size_t tableI);
 
