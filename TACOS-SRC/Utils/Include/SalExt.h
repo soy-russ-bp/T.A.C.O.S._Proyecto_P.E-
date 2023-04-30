bb5451ffaf5e875b54ed221f2fd326e603b2dcab
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2023-04-30 15:25:40 -0600

    SalExt: Add _Ptr_InNull_OutMaybeNull_, _Str_In_NotNull_

--- TACOS-SRC/Utils/Include/SalExt.h
+++ TACOS-SRC/Utils/Include/SalExt.h
@@ -4,3 +4,5 @@
 
 #define OnTrueReturn (return)
 #define OnFalseReturn (!return)
+#define SalExt_Ptr_InNull_OutMaybeNull_ _Deref_pre_null_ _Deref_post_maybenull_
+#define SalExt_Str_In_NotNull_ _In_z_ _Notnull_ const
