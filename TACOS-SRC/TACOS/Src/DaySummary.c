aa492cb5c8e464634e7b6cf8d8c0a86518dca802
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2023-04-30 02:19:30 -0600

    DaySummary: Replace hardcoded print

--- TACOS-SRC/TACOS/Src/DaySummary.c
+++ TACOS-SRC/TACOS/Src/DaySummary.c
@@ -3,18 +3,54 @@
 #include "ConsoleOut.h"
 #include "Orders.h"
 
+static void PrintHorizontalSeparatorCustom(TCHAR startCh, TCHAR middleA, TCHAR middleB, TCHAR endCh, bool newLine) {
+	ConsoleOut_WriteChar(startCh);
+	ConsoleOut_WriteCharRepeat(L'═', 43);
+	ConsoleOut_WriteChar(middleA);
+	ConsoleOut_WriteChar(middleB);
+	ConsoleOut_WriteCharRepeat(L'═', 43);
+	ConsoleOut_WriteChar(endCh);
+	if (newLine) {
+		ConsoleOut_NewLine();
+	}
+}
+
+static void PrintHorizontalSeparator2(TCHAR middleA, TCHAR middleB) {
+	PrintHorizontalSeparatorCustom(L'╠', middleA, middleB, L'╣', true);
+}
+
+static void PrintHorizontalSeparator(TCHAR middle) {
+	PrintHorizontalSeparatorCustom(L'╠', middle, middle, L'╣', true);
+}
+
+static void PrintCell(TSTR content, size_t width) {
+	ConsoleOut_WriteChar(L'║');
+	ConsoleOut_WriteAligned(content, width, ALIGN_CENTER);
+	ConsoleOut_WriteChar(L'║');
+}
+
 static void PrintTableHeader(void) {
-	ConsoleOut_WriteLine(_T("╠════════════════════════════════════════════════════════════════════════════════════════╣"));
-	ConsoleOut_WriteLine(_T("║                                     Resumen del día                                    ║"));
-	ConsoleOut_WriteLine(_T("╠═══════════════════════════════════════════╦╦═══════════════════════════════════════════╣"));
-	ConsoleOut_WriteLine(_T("║                  Ingresos                 ║║                  Ordenes                  ║"));
-	ConsoleOut_WriteLine(_T("╠═══════════════════════════════════════════╣╠═══════════════════════════════════════════╣"));
+	PrintHorizontalSeparator(L'═');
+	PrintCell(_T("Resumen del día"), 88);
+	ConsoleOut_NewLine();
+	PrintHorizontalSeparator(L'╦');
+	PrintCell(_T("Ingresos"), 43);
+	PrintCell(_T("Ordenes"), 43);
+	ConsoleOut_NewLine();
+	PrintHorizontalSeparator2(L'╣', L'╠');
+}
+
+static void PrintOptionInputRow(void) {
+	ConsoleOut_Write(_T("║ Opción:"));
+	ConsoleOut_WriteCharRepeat(' ', 80);
+	ConsoleOut_WriteChar(L'║');
+	ConsoleOut_NewLine();
 }
 
 static void PrintTableFooter(void) {
-	ConsoleOut_WriteLine(_T("╠═══════════════════════════════════════════╩╩═══════════════════════════════════════════╣"));
-	ConsoleOut_WriteLine(_T("║ Opción:                                                                                ║"));
-	ConsoleOut_Write/**/(_T("╚════════════════════════════════════════════════════════════════════════════════════════╝"));
+	PrintHorizontalSeparator(L'╩');
+	PrintOptionInputRow();
+	PrintHorizontalSeparatorCustom(L'╚', L'═', L'═', L'╝', false);
 }
 
 static void PrintFieldStart(TSTR fieldName) {
