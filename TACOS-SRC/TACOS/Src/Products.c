00204a3ad61e28df03eaddcd683c4c2e3e9a8b0a
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2023-05-01 23:42:47 -0600

    Products: Complete product array

--- TACOS-SRC/TACOS/Src/Products.c
+++ TACOS-SRC/TACOS/Src/Products.c
@@ -1,9 +1,14 @@
-#include "Products.h"
+﻿#include "Products.h"
 #include "ArrayUtils.h"
 
 static const ProductInfo Products[] = {
-	{ _T("TAP"), _T("Tacos de pastor"), 15 },
-	{ _T("TDA"), _T("Tacos de asada"), 20 }
+	{ _T("TAP"), _T("Tacos de pastor"),		15.00 },
+	{ _T("TAA"), _T("Tacos de asada"),		17.50 },
+	{ _T("TAB"), _T("Tacos de barbacoa"),	20.00 },
+	{ _T("TBI"), _T("Tacos de birriaa"),	16.00 },
+	{ _T("TEE"), _T("Té"),					12.50 },
+	{ _T("TAP"), _T("Horchata"),			10.00 },
+	{ _T("CEB"), _T("Cebada"),				14.50 }
 };
 
 const size_t Products_TypesCount = StaticLength(Products);
