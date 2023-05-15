43d5eaa2a6cff59136c022b5bbc5b6fe196fbbc6
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2023-05-15 15:40:39 -0600

    ArrayUtils: Add StaticStrLength

--- TACOS-SRC/Utils/Include/ArrayUtils.h
+++ TACOS-SRC/Utils/Include/ArrayUtils.h
@@ -8,6 +8,10 @@ bool Array_IsInBounds(size_t index, size_t size);
 // El arreglo debe ser estático.
 #define StaticLength(arr) (sizeof(arr)/sizeof(arr[0]))
 
+// Longitud de una cadena.
+// La cadena debe ser estático.
+#define StaticStrLength(str) (StaticLength(str) - 1)
+
 // Arreglo seguido de su tamaño, en forma de lista de argumentos.
 // El arreglo debe ser estático.
 #define StaticArrayAndLength(arr) arr, StaticLength(arr)
